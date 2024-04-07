3cf1a0dca9f4ea529f2d68fca7ed09439846b8cb
Ziemas <ziemas@ziemas.se>
2024-04-07 11:08:51 +0200

    Disable demo version check

--- linuxdoom-1.10/g_game.c
+++ linuxdoom-1.10/g_game.c
@@ -1432,11 +1432,14 @@ G_DoPlayDemo(void)
 
 	gameaction = ga_nothing;
 	demobuffer = demo_p = W_CacheLumpName(defdemoname, PU_STATIC);
-	if (*demo_p++ != VERSION) {
-		fprintf(stderr, "Demo is from a different game version!\n");
-		gameaction = ga_nothing;
-		return;
-	}
+
+	// skip version check
+	demo_p++;
+	//if (*demo_p++ != VERSION) {
+	//	fprintf(stderr, "Demo is from a different game version!\n");
+	//	gameaction = ga_nothing;
+	//	return;
+	//}
 
 	skill = *demo_p++;
 	episode = *demo_p++;
